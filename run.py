7c79e2d8ba0e822c62e0eb2f3585e55d4eabe1f8
VilmaTuresson <vilma.turesson@gmail.com>
2022-02-10 13:52:18 +0000

    create function to loop out a grid

--- run.py
+++ run.py
@@ -6,3 +6,14 @@ player_guess_board = [[' '] * 8 for i in range(8)]
 computer_board = [[' '] * 8 for i in range(8)]
 computer_guess_board = [[' '] * 8 for i in range(8)]
 
+
+def print_board(board):
+    """
+    Function to loop out a grid
+    """
+    print('  1 2 3 4 5 6 7 8')
+    row_num = 1
+    for row in board:
+        print('%d|%s|' % (row_num, '|'.join(row)))
+        row_num += 1
+
