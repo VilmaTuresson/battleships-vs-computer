742de02704cab7a87b35f8ceb1dec5178a279e34
VilmaTuresson <vilma.turesson@gmail.com>
2022-02-10 13:22:05 +0000

    creating 4 boards for guessing and setting ships for player and computer

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 from random import randint
 
+
+player_board = [[' '] * 8 for i in range(8)]
+player_guess_board = [[' '] * 8 for i in range(8)]
+computer_board = [[' '] * 8 for i in range(8)]
+computer_guess_board = [[' '] * 8 for i in range(8)]
+
